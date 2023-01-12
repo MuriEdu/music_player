86bfb98d8db255c1cca23d95b79ebe30bb0c4668
Murilo Ramos <muriloeduardo6@gmail.com>
2023-01-12 17:21:05 -0300

    player one update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Franguinho from "./assets/franguinho.jpg";
+import {
+  BsFillPauseFill,
+  BsFillPlayFill,
+  BsFillSkipBackwardFill,
+  BsFillSkipForwardFill,
+} from "react-icons/bs";
 
 function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const getButton = (isPlaying) => {
+    if (isPlaying) {
+      return <BsFillPauseFill className="buttonIcons" />;
+    } else {
+      return <BsFillPlayFill className="buttonIcons" />;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Content">
+        <div id="PlayerOne" className="Player">
+          <div className="Image">
+            <img
+              className="Image"
+              id="ImageOne"
+              src={Franguinho}
+              alt="Img"
+            ></img>
+          </div>
+          <div className="Text">
+            <p id="Name">Franguinho na Panela</p>
+            <p id="Author">Lourenço e Lorival</p>
+          </div>
+          <div className="buttons">
+            <button className="buttonsComponent">
+              {<BsFillSkipBackwardFill className="buttonIcons" />}
+            </button>
+            <button
+              className="buttonsComponent"
+              onClick={() => {
+                setIsPlaying(!isPlaying);
+              }}
+            >
+              {getButton(isPlaying)}
+            </button>
+            <button className="buttonsComponent">
+              {<BsFillSkipForwardFill className="buttonIcons" />}
+            </button>
+            <div>
+              <audio src="./songs/Franguinho Na Panela   Lourenço u0026 Lourival  (2002[1].mp3"></audio>
+            </div>
+          </div>
+        </div>
+        <div className="PlayerH">
+          <div id="PlayerTwo" className="Player">
+            2
+          </div>
+          <div id="PlayerThree" className="Player">
+            3
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
